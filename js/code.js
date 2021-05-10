20aa1840242444d96a5ebc820f012e4fb8f40ac9
Ayushman09 <ayushmanchak@gmail.com>
2021-05-10 10:48:28 +0530

    Assignment

--- js/code.js
+++ js/code.js
@@ -1,26 +1,25 @@
-
-
-
 function validateEmail() {
-    const inputEmail = document.getElementById("Email");
+    var inputEmail = document.getElementById("email").value;
     console.log(inputEmail);
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if (inputEmail.value.match(mailformat)) {
-        return true;
+        alert("Email: Good to go!");
+        //return true;
     } else {
-        alert("You have entered an invalid email address!");
-        return false;
+        alert("Email: invalid email address!");
+        //return false;
     }
 }
 
 function checkPassword() {
-    const inputPassword = document.getElementById("pwd");
+    var inputPassword = document.getElementById("pswd").value;
     console.log("Working");
     var valid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
     if (inputPassword.value.match(valid)) {
-        return true;
+        alert("Password: Good to go!");
+        //return true;
     } else {
         alert('Password should be: 8 to 15 characters which contain at least one lowercase letter, one uppercase letter, one numeric digit, and one special character')
-        return false;
+        //return false;
     }
 }
